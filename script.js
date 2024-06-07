768ad7667ca38c022513830cac9bcf61b0fde68b
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-07 19:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+  
+let target = document.getElementById("level");
+
+let level = 1;
+while(target.parentElement != null)
+{
+    level++;
+    target = target.parentElement;
+}
+
+alert(level);
